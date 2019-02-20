3b22a9a1414390190734ea1c914c221549320040
v.vayavodzin <v.vayavodzin@godeltech.com>
2019-02-20 12:26:44 +0300

    Add love triangle logic;

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let totalLovers = 0;
+  let arr = [...preferences];
+  arr.unshift(0);
+
+  for (let i = 1; i <= arr.length; i++) {
+    recursion(counted = [], i);
+  }
+
+  function recursion(counted, i) {
+    let lover = arr[i];
+    if (counted.length < 3 && lover && lover != i) {
+      counted.push(lover);
+      return recursion(counted, lover);
+    } else if (counted.length == 3 && counted[0] == lover) {
+      counted.map(x => arr[x] = 0);
+      totalLovers++
+    }
+  }
+
+  return totalLovers;
 };
